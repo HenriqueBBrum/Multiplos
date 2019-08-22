92ee4643dc24b740189e66bcf153b5fe07d1a9cf
Henrique Becker Brum <45078469+HenriqueBBrum@users.noreply.github.com>
2019-08-22 00:56:04 -0300

    Add files via upload

--- PAA-Pesquisa Bin.c
+++ PAA-Pesquisa Bin.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
 
+
+char bin(int* vet, int init, int end, int num){
+    if(end<=init)
+        return 'n';
+    int mid =(end+init)/2;
+
+    if(num==vet[mid]){
+        return 's';
+    }else if(num>vet[mid]){
+        return bin(vet,mid+1,end, num);
+    }else {
+        return bin(vet,init,mid, num);
+    }
+
+}
+
+char pesquisa_bin(int* vetor, int n, int num){
+
+    char result = bin(vetor,0,n-1,num);
+    if(result =='s'){
+
+    }
+
+}
+
+int main(){
+    int vetor[] = {1,2,3,4,5,7,8,9};
+    int  r = pesquisa_bin(vetor,9,);
+    printf("%d\n", r);
+
+
+}
