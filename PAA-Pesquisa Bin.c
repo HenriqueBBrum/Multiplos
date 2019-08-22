813323e6393ec63c2dede2439e7875f182737f1a
Henrique Becker Brum <45078469+HenriqueBBrum@users.noreply.github.com>
2019-08-22 00:59:37 -0300

    Add files via upload

--- PAA-Pesquisa Bin.c
+++ PAA-Pesquisa Bin.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 
-char bin(int* vet, int init, int end, int num){
+int bin(int* vet, int init, int end, int num){
     if(end<=init)
-        return 'n';
+        return -1;
     int mid =(end+init)/2;
 
     if(num==vet[mid]){
-        return 's';
+        return mid;
     }else if(num>vet[mid]){
         return bin(vet,mid+1,end, num);
     }else {
@@ -16,19 +16,19 @@ char bin(int* vet, int init, int end, int num){
 
 }
 
-char pesquisa_bin(int* vetor, int n, int num){
+int pesquisa_bin(int* vetor, int n, int num){
 
-    char result = bin(vetor,0,n-1,num);
-    if(result =='s'){
-
-    }
+    int result = bin(vetor,0,n-1,num);
+    return result;
 
 }
 
 int main(){
     int vetor[] = {1,2,3,4,5,7,8,9};
-    int  r = pesquisa_bin(vetor,9,);
-    printf("%d\n", r);
+    int num = 7;
+    int n = 9;
+    int  r = pesquisa_bin(vetor,n,num);
+    printf("posicao do num %d = %d\n",num, r);
 
 
 }
